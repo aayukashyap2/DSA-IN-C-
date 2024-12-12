e0be893e7547a00b3fc1bc6141f7de623092e6fe
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-13 01:27:12 +0530

    initial commit

---
+++ BASICS OF C_++/ModulusOperator.cpp
@@ -0,0 +1,13 @@
+/*Take two integers a and b : a>b , and find the
+remainder when a is divided by b*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 12;
+    int b = 5;
+    int c = a % b;
+    cout << c;
+    //cout<< a % b;
+}
+// NOTE: if a > b then a % b = a .
