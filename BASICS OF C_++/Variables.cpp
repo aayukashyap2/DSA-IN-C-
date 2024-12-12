0c03467943a75bc262a6036387d5da195015ec8e
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-13 00:07:51 +0530

    Initial commit

---
+++ BASICS OF C_++/Variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x=5;
+    cout<< x << endl;
+    x = 7;
+    cout<< x << endl;
+    x += 8;
+    cout<< x << endl;
+    x =-25;
+    cout<< x << endl;
+}
