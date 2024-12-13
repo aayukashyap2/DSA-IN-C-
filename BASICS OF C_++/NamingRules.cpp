bb470b043ca796fc4eddea8da0b3b6e4a0159da4
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-13 16:55:26 +0530

    initial commit

---
+++ BASICS OF C_++/NamingRules.cpp
@@ -0,0 +1,35 @@
+//naming rules in c++
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 4;
+    cout<<x <<endl;
+
+    int _x1 = 5;
+    cout<<_x1 << endl;
+
+    int $x174 = 6;
+    cout<<$x174 << endl;
+
+    int Aayu = 7;
+    cout<<Aayu << endl;   //variable can start from an alphabet , underscore(_) and $ only.
+
+   /* int 1x = 8;
+    cout<< 1x<< endl;
+
+    int int = 9;
+    cout<< int<< endl;   //some particular keywords are not allowed 
+
+    int float = 10;
+    cout<< float<< endl; */ 
+
+    int float1 = 11;
+    cout<< float1<< endl;
+
+    // int x  y = 12;
+    // cout<< x  y<< endl; 
+
+    // int x,y = 12;
+    // cout<< x,y<< endl;     // commas and blank spaces are not allowed 
+}
