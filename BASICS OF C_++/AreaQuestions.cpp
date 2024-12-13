5620b6ff8a3ef067cd34f417b285312e33297e77
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-13 16:33:55 +0530

    initial commit

---
+++ BASICS OF C_++/AreaQuestions.cpp
@@ -0,0 +1,13 @@
+/*Find the area of circle :
+float radius = 5
+float pi = 3.14 
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    float radius = 6;
+    float pi = 3.14;
+    float area = pi * radius * radius;
+    cout << area;
+    //cout << pi * radius * radius;
+}
