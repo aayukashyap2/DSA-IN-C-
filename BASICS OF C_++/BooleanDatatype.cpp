124f93aef1490b5818f5ef51527bc5572dced51b
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-13 17:39:01 +0530

    initial commit

---
+++ BASICS OF C_++/BooleanDatatype.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main () {
+    bool Gun = true;
+    cout<< Gun << endl;
+
+    bool Humanity = false;
+    cout << Humanity << endl;
+
+    bool apple = 0;
+    cout<< apple << endl;    // always give "zero" for zero quantity
+
+    bool banana = 5;
+    cout<< banana<< endl;
+ 
+    bool grains = 12.5;      // always give "one" for Non zero quantity
+    cout<< grains << endl;
+
+}
