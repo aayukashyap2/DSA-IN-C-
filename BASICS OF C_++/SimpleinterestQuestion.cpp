683c71524c290892ba14da9ad73cde6c16dfa875
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-13 17:15:30 +0530

    initial commit

---
+++ BASICS OF C_++/SimpleinterestQuestion.cpp
@@ -0,0 +1,18 @@
+/*Calculating Simple Interest
+float p, r, t, si
+p = 100
+r = 10 
+t = 2
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    float p = 100, r = 10;
+    float t = 2;
+
+    float si = ( p * r * t)/ 100;
+    cout<< si;
+
+
+}
