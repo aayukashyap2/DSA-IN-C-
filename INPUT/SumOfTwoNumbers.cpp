500f406e18320de1ca8774f360dd541738f49680
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-15 00:07:40 +0530

    initial commit

---
+++ INPUT/SumOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+    cout<< "Enter the 1st no. = ";
+    cin >> x ;
+
+    int y;
+    cout<< "Enter the 2nd no. = ";
+    cin >> y ;
+
+    int sum = x + y ;
+    cout << "Sum of the two numbers are = " ;
+    cout << sum ;
+}
