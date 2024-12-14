594d108f3a8583b5b29ba7cb3288a5ec2b3ebc9c
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-14 22:52:01 +0530

    intial commit

---
+++ BASICS OF C_++/Question2.cpp
@@ -0,0 +1,10 @@
+/* Print 10 using numbers less than 6 in c++*/
+
+#include<iostream>
+using namespace std;
+int main (){
+    int x = 1 + 2 + 3 + 4 ;
+    //int x = 1 + 4 + 5 ;
+    //int x = 5 + 5 ;
+    cout<< x ;
+}
