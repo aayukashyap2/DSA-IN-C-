0dc1633e353d2fafb716ce6a2dbf9ddc6446b459
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-14 23:51:01 +0530

    initial commit

---
+++ INPUT/SquareOfGivenNumber.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x ;
+    cout<< "Enter the given number = ";
+    cin>> x;
+    
+    int square = x * x ;
+    cout << "square of the given number is = ";
+    cout << square ;
+
+
+}
