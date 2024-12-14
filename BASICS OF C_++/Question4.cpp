d5e6c91b9c959dc815aa98c08444be9b6180cc07
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-14 23:16:47 +0530

    initial commit

---
+++ BASICS OF C_++/Question4.cpp
@@ -0,0 +1,14 @@
+/*WAP to find the circumference of a circle with radius 10 in c++*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int r = 10;
+    float pi = 3.14;
+    float circumference = 2 * pi * r;
+    cout<<"circumference of a circle = "<< circumference ;
+    //cout<< circumference;
+
+
+
+}
