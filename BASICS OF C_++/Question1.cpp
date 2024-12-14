f66ae1baca2fc073bbd2af25be24eae85009a8f5
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-14 22:39:07 +0530

    initial commit

---
+++ BASICS OF C_++/Question1.cpp
@@ -0,0 +1,10 @@
+/* How can you output "AAYUSH" and "KASHYAP" in two different lines in c++ ?*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"AAYUSH"<<endl;
+    cout<<"KASHYAP"<<endl;
+
+   // or  cout<<"AAYUSH"<<endl<<"KASHYAP";
+}
