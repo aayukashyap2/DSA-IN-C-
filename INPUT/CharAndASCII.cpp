53dafed214a314f10653c2e56eb4aae479faebc4
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-15 21:45:18 +0530

    initial commit

---
+++ INPUT/CharAndASCII.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char _1st = '1';
+    char _2nd = 'h';
+    char ch = 'P';
+    char _4th = 'A';
+    char _5th = '%';
+    char _6th = '#';
+    char _7th = '@';  // only single letter is considered as character.
+    cout<<ch<<endl;
+    cout<<_4th<<" "<<_1st <<endl;
+    
+    //ASCII__VALUES
+
+    char ascii = 'e';
+    char _ascii = 'D';
+    char __ascii = '0';
+    cout<<(int) ascii <<endl << (int)_ascii << endl << (int)__ascii;
+
+
+}
