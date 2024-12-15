2302dcb478df79811c02ce87352b1e22e2717b8b
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-15 23:01:38 +0530

    initial commit

---
+++ INPUT/HalfOfInteger.cpp
@@ -0,0 +1,14 @@
+/*Take integer as input and print half of the number*/
+
+#include<iostream>
+using namespace std;
+int main(){
+ int x ;
+ cout<<"Enter the integer = ";
+ cin>>x;
+
+ float y = (float)x;
+ cout<<y/2<<endl;
+
+
+}
