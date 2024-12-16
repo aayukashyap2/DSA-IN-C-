7ae2b8b4b98e44527a203f126e830edbcaff5e41
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-16 23:46:22 +0530

    intial commit

---
+++ COnditional STatements/Question2.cpp
@@ -0,0 +1,15 @@
+//Given an integer.Print the absolute value of that integer.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter an integer = ";
+    cin>>n;
+    if(n>0){
+        cout<<n;
+    } 
+    else{
+        cout<<-n;
+    }
+}
