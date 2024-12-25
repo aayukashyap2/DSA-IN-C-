f35447ce35688b1b4f685508f573de027de3caca
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-25 20:18:57 +0530

    initial commit

---
+++ TernaryandSwitch/Question11.cpp
@@ -0,0 +1,13 @@
+//predict the output
+
+#include<iostream>
+using namespace std ;
+int main(){
+    int i = 65 ;
+    char j = 'A';
+    if (i == j)
+    cout<< "A stands for Aayush kashyap ";
+    else
+    cout<<"A stands for Aayu";
+    return 0;
+} 
