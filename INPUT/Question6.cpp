ea2cde23fe60d193948d93061dc76cef925dc0c3
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-26 21:46:39 +0530

    initial commit

---
+++ INPUT/Question6.cpp
@@ -0,0 +1,15 @@
+/*WAP for finding the volume of cylinder by taking radius and height as input*/
+
+#include<iostream>
+using namespace std;
+int  main(){
+    int r,h;
+    float pi = 3.14;
+    cout<<"Enter the radius = ";
+    cin>>r;
+    cout<<"Enter the height = ";
+    cin>>h;
+    float v = 2*pi*r*h;
+    cout<<"Required Volume is = "<<v;
+
+}
