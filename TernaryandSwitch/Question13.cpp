51a8e71b7c8799a3fa3d66d90a932c1472631c55
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-28 23:53:13 +0530

    initial commit

--- TernaryandSwitch/Question13.cpp
+++ TernaryandSwitch/Question13.cpp
@@ -1,10 +1,13 @@
 /*Predict the Output*/
-
+#include<iostream>
+using namespace std;
 int main(){
     int a = 5 ,b,c;
+    
 
     b = a = 15;
     c = a < 15;
+    
 
     cout<<"a = " << a << " , b = " << b << " , c ="<<c;
 
