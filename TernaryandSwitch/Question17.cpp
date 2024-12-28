90e5dfa086464df62a096df12546b2bccfed04d8
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-29 00:20:34 +0530

    initial commit

---
+++ TernaryandSwitch/Question17.cpp
@@ -0,0 +1,12 @@
+//Predict the output
+#include<iostream>
+using namespace std;
+int main (){
+    int number = -4;
+    char result ;
+    result = number > 0 ? 'p' : 'N' ;
+    
+    cout<< result << endl ;
+    
+    return 0;
+}
