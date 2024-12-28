769877b3b7d072bc2b39b6050ad422175d57fbfa
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-28 23:58:53 +0530

    initial commit

---
+++ TernaryandSwitch/Question15.cpp
@@ -0,0 +1,13 @@
+//Predict the output
+#include<iostream>
+using namespace std;
+int main (){
+    int test = 0;
+    
+
+    cout<<"First character " << '1' << endl;
+
+    cout << "second character " << (test ? 3 : '1') << endl;
+
+    return 0;
+}
