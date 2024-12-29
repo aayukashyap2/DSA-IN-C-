6f0ce9c2f068f53bf61139ac606155b68c729137
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-29 23:34:51 +0530

    initial commit

--- Loops/Question3.cpp
+++ Loops/Question3.cpp
@@ -3,7 +3,16 @@
 #include<iostream>
 using namespace std;
 int main(){
+    //100 times loops
     for (int i=1;i<=100;i++){
+           if(i%2==0) {
+            cout<<i<<" ";
+           }
+    }
+
+
+    //50 times loops
+    for (int i=2;i<=100;i=i+2){
            if(i%2==0) {
             cout<<i<<" ";
            }
