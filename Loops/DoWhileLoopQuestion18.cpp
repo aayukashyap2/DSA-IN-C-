c43e3e95db84ff9f2c9bec0fc48bbe1c379b0177
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-31 12:34:29 +0530

    initial commit

---
+++ Loops/DoWhileLoopQuestion18.cpp
@@ -0,0 +1,11 @@
+/*WAP to Demonstrate Do while loop*/
+
+#include <iostream>
+using namespace std;        
+int main(){
+    int i = 11;   ///atleast one time it will run
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=10);
+}
