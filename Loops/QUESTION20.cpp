2de32d8f605a6694927ab068f9e8bc1d3133c10c
aayukashyap2 <kashyapayush681@gmail.com>
2024-12-31 22:51:53 +0530

    initial commit

---
+++ Loops/QUESTION20.cpp
@@ -0,0 +1,14 @@
+/*Predict the output*/
+
+//INFINITE LOOP
+
+#include <iostream>
+using namespace std;    
+int main(){
+    int i ;
+    while (i=10) {   //assign
+        cout<<i<<endl;
+        i=i+1;
+    }
+ }
+ 
