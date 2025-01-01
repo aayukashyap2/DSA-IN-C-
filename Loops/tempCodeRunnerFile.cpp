bba2128d515767ce85151488a235e37a16b409b5
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-01 20:42:51 +0530

    initial commit

---
+++ Loops/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+int main(){
+    int x = 4,y =0;
+    while (x >= 0){
+        x--;
+        y++;
+        if (x == y)
+            continue;
+            else
+            cout << x << " " << y << endl;
+    }
+}
