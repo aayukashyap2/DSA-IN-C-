5c9291ba89f008a04c5329d714ba93d5bed1d45e
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-02 21:33:47 +0530

    initial commit

---
+++ PatternPrintingQuestions/SolidRectangle.cpp
@@ -0,0 +1,23 @@
+/*Print the given pattern
+          ***** 
+          *****
+          *****
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter the number of rows =";
+    cin>>m;
+    int n;
+    cout<<"Enter the number of columns =";
+    cin>>n;
+    for(int i=1;i<=m;i++){ //rows = m
+        for(int j=1;j<=n;j++){ //columns = n
+            cout<<"*";
+        }
+        cout<<endl;
+    } 
+
+}
