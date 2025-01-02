713a9229ef84f1ea33dc21f545a5ee78bbaea0cf
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-02 20:41:10 +0530

    initial commit

--- QuestionsUsingOperators/Factorial.cpp
+++ QuestionsUsingOperators/Factorial.cpp
@@ -6,17 +6,12 @@ int main(){
     int n;
     cout<<"Enter a number =";
     cin>>n;
-    int sum = 0;
-    int r = 0;
-    while (n != 0){
-        int ld = n%10;
-        r *= 10;
-        r += ld;
-        n /= 10;
-        sum += ld;
+    int fact = 1;
+    for (int i = 1; i <= n; i++){
+        fact *= i;
     }
-    cout<<"sum= "<<sum<<endl;
-    cout<<"reverse = "<<r;
+    cout<<"Factorial= "<<fact;
+     
 
 }  
  
