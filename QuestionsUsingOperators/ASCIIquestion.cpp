62a9871d42f3472862001dea719e27acd6506f65
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-02 21:14:57 +0530

    initial commit

---
+++ QuestionsUsingOperators/ASCIIquestion.cpp
@@ -0,0 +1,12 @@
+/*WAP to print all the ASCII values and their equivalent characters of 26
+alphabets using a while loop.*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    char ch = 'A';
+    while (ch <= 'Z'){
+        cout<<ch<<" = "<<int(ch)<<endl;
+        ch++;
+    }
+}
