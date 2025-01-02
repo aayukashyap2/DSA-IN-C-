67de6eed987d33d19b8dedcf0769116c5d6179fe
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-02 21:56:36 +0530

    initial commit

---
+++ PatternPrintingQuestions/SameNumberSquare.cpp
@@ -0,0 +1,22 @@
+/*Print the given pattern
+          1 1 1 1 1
+          2 2 2 2 2
+          3 3 3 3 3
+          4 4 4 4 4
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int n;
+    cout<<"Enter the side of square =";
+    cin>>n;
+    for(int i=1;i<=n;i++){ //rows = n
+        for(int j=1;j<=n;j++){ //columns = n
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    } 
+
+}
