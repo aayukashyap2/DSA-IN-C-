67345e3f418b7084ced619d3fc517c869fede6fd
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-03 23:07:39 +0530

    initial commit

---
+++ PatternPrintingQuestions/NumberTriangle.cpp
@@ -0,0 +1,22 @@
+/*Print the given pattern
+         1
+         1 2
+         1 2 3
+         1 2 3 4
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the side of square =";
+    cin>>n;
+  
+    for(int i=1;i<=n;i++){ //rows = n
+        for(int j=1;j<=i;j++){ //columns = n
+            cout<<j<<" ";
+        }
+        cout<<endl;  
+    } 
+
+}
