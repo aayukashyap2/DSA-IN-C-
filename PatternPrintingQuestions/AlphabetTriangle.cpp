56cff619dd527f9d7a5ffcbc48fa517f9f0d34ae
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-03 23:11:02 +0530

    initial commit

---
+++ PatternPrintingQuestions/AlphabetTriangle.cpp
@@ -0,0 +1,22 @@
+/*Print the given pattern
+         A
+         A B
+         A B C
+         A B C D
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the side of square =";
+    cin>>n;
+  
+    for(int i=1;i<=n;i++){ //rows = n
+        for(int j=1;j<=i;j++){ //columns = n
+            cout<<char(j + 64)<<" ";
+        }
+        cout<<endl;  
+    } 
+
+}
