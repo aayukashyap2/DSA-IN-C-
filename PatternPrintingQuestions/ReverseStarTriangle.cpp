78ae58831a387ef7267d6481a70252d8a1d42932
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-04 23:18:03 +0530

    initial commit

--- PatternPrintingQuestions/ReverseStarTriangle.cpp
+++ PatternPrintingQuestions/ReverseStarTriangle.cpp
@@ -8,8 +8,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    for(int i=4;i>0;i--){
-        for(int j=0;j<i;j++){
+    int n;
+    cout<<"Enter the number of rows =";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
             cout<<"*";
         }
         cout<<endl;
