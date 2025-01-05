ec36af6daf2cc1e6a0bfe4d91baec38459586bbd
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-05 23:35:30 +0530

    initial commit

---
+++ PatternPrintingQuestions/Rhombus.cpp
@@ -0,0 +1,20 @@
+/*Print the  pattern of Rhombus*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the side of square =";
+    cin>>n;
+  
+    for(int i=1;i<=n;i++){ //rows = n
+        for(int j=1;j<=n-i;j++){ //columns = n,,,,spaces loops
+            cout<<"  ";
+        }    
+        for(int j=1;j<=n;j++){ //columns = n,,,,stars loops
+            cout<<"* ";
+        }
+        cout<<endl;  
+    } 
+
+}
