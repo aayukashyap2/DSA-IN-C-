8a4343af2a75fcfc4d7d62de4fb1006c443a4a46
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-06 18:49:41 +0530

    initial commit

---
+++ Functions/BasicFunction2.cpp
@@ -0,0 +1,19 @@
+//understanding Arguments
+
+#include<iostream>
+using namespace std;
+void startriangle(int x){
+    for (int i =1; i<=x; i++){
+        for (int j =1; j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    startriangle(3);
+    cout<<"Hello AAYU"<<endl;
+    startriangle(4);
+    startriangle(5);
+}
