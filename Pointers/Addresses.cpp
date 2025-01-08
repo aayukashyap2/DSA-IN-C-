415029a7d0898fc990179a522c607f3e004b4b43
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-08 21:22:53 +0530

    initial commit

---
+++ Pointers/Addresses.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int x;  //0x61ff0c
+    //int x = 9;
+    //char x = 's';   //sÇ
+    float x = 0.7;   //0x61ff0c
+    cout << &x;    
+}
