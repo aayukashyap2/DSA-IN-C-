2eab68fd4c4ae9ed10cb14cf853734304409a7d3
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-08 21:04:19 +0530

    intial commit

---
+++ Pointers/revisitingvariables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<sizeof(int)<<endl;   //in bytes always and 1 byte = 32 bits and formula = 2^n 
+    cout<<sizeof(char)<<endl;
+    cout<<sizeof(bool)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(long long)<<endl;
+    cout<<sizeof(double)<<endl;
+}
