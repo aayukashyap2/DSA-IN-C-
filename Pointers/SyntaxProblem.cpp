9ae555eb987d5be38878dd74170c62a3a65fb703
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-09 20:41:54 +0530

    initial commit

---
+++ Pointers/SyntaxProblem.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 12, y = 10;;
+    //int* p1 = &x , p2 = y;       not valid 
+     
+    int* p2 = &x;             //valid so its good to assign pointer variable one by one
+    
+}
