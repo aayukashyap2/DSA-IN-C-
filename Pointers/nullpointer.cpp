1aef344fe208ade9c6d06e3158bfa7c2342aa926
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-09 22:36:21 +0530

    initial commit

---
+++ Pointers/nullpointer.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int* ptr = NULL;  // NULL -> 0 // NULL -> '\0'  //Reserved address
+    cout<<ptr;   //0 
+
+    // '\0' -> null character
+   
+   
+   // char ch ='a'
+    //cout<<(int)ch;
+}
