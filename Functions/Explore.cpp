cd090763126d3fbb0de5c67473297d28a961281e
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-10 22:43:01 +0530

    initial commit

---
+++ Functions/Explore.cpp
@@ -0,0 +1,3 @@
+/*Can some function name be used for different functions  without any conflict?
+
+Answer =  Yes but there must be some differentiating factor like return type ,Data type of parameters,Count of parameters*/
