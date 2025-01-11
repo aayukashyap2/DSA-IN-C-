bb1d4898ee48636b1c0e5c53c5157f25a05d1780
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-11 23:38:42 +0530

    initial commit

---
+++ Array1/OutputAndInput1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7];
+    // cin>>arr[0];
+    // cin>>arr[1];    // By this You can also Take The Input
+    
+    // Input
+    for (int i = 0; i<=6; i++){
+        
+        cin>>arr[i];
+
+    }
+
+
+    // Output
+    for (int i = 0; i<=6; i++){
+        cout<<arr[i]<<" ";
+
+    }
+}
