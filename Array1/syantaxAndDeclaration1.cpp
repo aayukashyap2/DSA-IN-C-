50b2cf8c61fa1b936fe99016a19038fd789ae9a1
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-11 23:21:04 +0530

    initial commit

---
+++ Array1/syantaxAndDeclaration1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[7] = {1,5,9,4,6,3,1}; // 7 boxes created and its indexing is 0 , 1 , 2 , 3 , 4 , 5 , 6.
+   
+    cout<<arr[0]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
+    cout<<arr[3]<<endl;
+    cout<<arr[4]<<endl;
+    cout<<arr[5]<<endl;
+    cout<<arr[6]<<endl;
+
+}
