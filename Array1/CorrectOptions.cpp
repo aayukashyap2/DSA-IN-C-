5192aec686c44fabaccaee13f220eeb3f0f9c7da
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-13 22:30:24 +0530

    initial commit

---
+++ Array1/CorrectOptions.cpp
@@ -0,0 +1,7 @@
+/* Are the following array declarations correct ?
+a> int a(25);   wrong -> int a[25] right
+b> int size = 10, b[size];    correct
+c> int c = { 0,1,2};     wrong -> int c[] = {0,1,2};
+*/
+
+// Answer = option a>.
--- Array1/markslessthan351st.cpp
+++ Array1/markslessthan351st.cpp
@@ -4,10 +4,10 @@ print its roll number.[roll number here refers to the index of the array.]*/
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
+    int x;
     cout<<"Enter the Number of students = ";
-    cin>>n;
-    int marks[n];  //index from 0 to n-1
+    cin>>x;
+    int marks[ n ];  //index from 0 to n-1
     cout<<"Enter the marks = ";
     // input
     for (int i = 0 ; i<= n- 1;i++){
