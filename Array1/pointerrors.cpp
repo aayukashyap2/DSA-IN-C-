227931aa6dba7176e6b3bb425f43d8079ad1afe0
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-13 23:07:10 +0530

    initial commit

---
+++ Array1/pointerrors.cpp
@@ -0,0 +1,15 @@
+/* Point out the errors (if any) in the following code : */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cin>>size;
+    int arr[size];
+    for (int i =1;i<=size ; i++){
+        cin>>arr[i];
+        cout<<arr[i];
+
+    }
+    return 0;
+}
