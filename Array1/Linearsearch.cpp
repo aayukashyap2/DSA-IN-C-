7f668755ea184a61c0a09a5f9ce3b20d1150ecbd
aayukashyap2 <kashyapayush681@gmail.com>
2025-01-14 22:42:16 +0530

    initial commit

---
+++ Array1/Linearsearch.cpp
@@ -0,0 +1,28 @@
+/* Find the element X in the array . Take Array and X as input. */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of array = ";
+    cin>>n;
+    int arr[n];
+    
+    // input
+    for(int i =0; i<=n-1; i++){
+        cin>>arr[i];
+    }
+    int x;
+    cout<<"Enter the element you want to search =";
+    cin>>x;
+
+    // Search
+    // check mark
+    bool flag = false;   // false -> not present 
+    for (int i = 0; i<=n-1; i++){
+        if(arr[i]==x)  flag = true;
+    }
+    if(flag == true ) cout<<"element found";
+    else cout<<"404 element not found";
+    
+}
